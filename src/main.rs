fe9b7bd3a166153b083d808e94287853378c3348
Antoine <me@antoinebellanger.fr>
2024-03-15 15:18:58 +0100

    feat: cli flag 'address'

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,10 @@ struct Args {
     /// Port to listen on
     #[arg(short, long, env, default_value_t = 5050)]
     port: u16,
+
+    /// Address to listen on
+    #[arg(short, long, env, default_value = "0.0.0.0")]
+    address: String
 }
 
 #[tokio::main]
@@ -14,7 +18,7 @@ async fn main() -> anyhow::Result<()> {
     let args = Args::parse();
 
     let app = Router::new().route("/", get(hello_world));
-    let listener = tokio::net::TcpListener::bind(("0.0.0.0", args.port)).await?;
+    let listener = tokio::net::TcpListener::bind((args.address, args.port)).await?;
 
     println!("Listening on http://{:?}", listener.local_addr()?);
     axum::serve(listener, app).await?;
