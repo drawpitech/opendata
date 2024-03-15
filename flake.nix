0fc9f8b761d40708c4b69c466fd9456891ce4831
Clément <clement2104.boillot@gmail.com>
2024-03-15 13:43:31 +0100

    feat: create project

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustup
+            nodejs
+          ];
+          RUSTC_VERSION = "stable-x86_64-unknown-linux-gnu";
+          # https://github.com/rust-lang/rust-bindgen#environment-variables
+          shellHook = ''
+            export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
+            export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
+          '';
+        };
+
+        formatter = pkgs.nixpkgs-fmt;
+      });
+}
