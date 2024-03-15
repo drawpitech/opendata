49970d33c2772856ad5ce1949b0849eedea43ffb
Baptiste Girardeau <contact@bgirardeau.me>
2024-03-15 15:01:16 +0100

    feat: cli flags

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,22 @@
-use std::env;
-
-use anyhow::Context;
 use axum::{routing::get, Router};
+use clap::Parser;
+
+/// Palachias backend server
+#[derive(Parser, Debug)]
+struct Args {
+    /// Port to listen on
+    #[arg(short, long, env, default_value_t = 5050)]
+    port: u16,
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    dotenvy::dotenv().ok();
-
-    let port = match env::var("PORT") {
-        Ok(port) => port.parse().context("invalid port")?,
-        Err(_) => 5050,
-    };
+    let args = Args::parse();
 
     let app = Router::new().route("/", get(hello_world));
-    let listener = tokio::net::TcpListener::bind(("0.0.0.0", port)).await?;
+    let listener = tokio::net::TcpListener::bind(("0.0.0.0", args.port)).await?;
 
-    println!("Listening on http://{}", listener.local_addr()?);
+    println!("Listening on http://{:?}", listener.local_addr()?);
     axum::serve(listener, app).await?;
 
     Ok(())
